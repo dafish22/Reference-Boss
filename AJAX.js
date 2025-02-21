dc04284f48d8e39ca497befdd002716f42ccaed1
Ieuan <123741133+dafish22@users.noreply.github.com>
2025-02-21 13:13:27 +0000

    AJAX

---
+++ AJAX.js
@@ -0,0 +1,6 @@
+// Hit API, request data blob.
+// Scrape data blob for authors, title, publisher, publication date and location.
+// Maybe grab book cover thumbs for nice picture graphics, also users can identify their source quicker.
+
+// items[0].volumeInfo.title
+// items[0].volumeInfo.authors[0]
